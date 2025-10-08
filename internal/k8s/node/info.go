3e28cab556bfa137847c1d617472119243ae85fb
Artur Malchanau <arturmolchanov@gmail.com>
2025-10-08 20:21:57 +0300

    Add allocatable node resources (#133)
    * k8s.nodes.allocatable_ephemeral_storage
    * k8s.nodes.allocatable_pods

--- internal/k8s/node/info.go
+++ internal/k8s/node/info.go
@@ -8,25 +8,27 @@ type Info struct {
 	Name   string            `db:"name"`
 	Labels map[string]string `db:"labels"`
 
-	Address                 map[string]string `db:"address"`
-	Age                     time.Duration
-	AllocatableCPU          float64           `db:"allocatable_cpu"`
-	AllocatableMemory       float64           `db:"allocatable_memory"`
-	Annotations             map[string]string `db:"annotations"`
-	Architecture            string            `db:"architecture"`
-	CapacityCPU             float64           `db:"capacity_cpu"`
-	CapacityMemory          float64           `db:"capacity_memory"`
-	ContainerRuntimeVersion string            `db:"container_runtime_version"`
-	CPUUtilisation          float64           `db:"cpu_utilisation"`
-	CreationTimestamp       time.Time         `db:"creation_ts"`
-	Images                  ImageList
-	InstanceType            string  `db:"instance_type"`
-	KernelVersion           string  `db:"kernel_version"`
-	KubeletVersion          string  `db:"kubelet_version"`
-	MemoryUtilisation       float64 `db:"memory_utilisation"`
-	OperatingSystem         string  `db:"operating_system"`
-	OSImage                 string  `db:"os_image"`
-	Taints                  TaintList
+	Address                     map[string]string `db:"address"`
+	Age                         time.Duration
+	AllocatableCPU              float64           `db:"allocatable_cpu"`
+	AllocatableEphemeralStorage int64             `db:"allocatable_ephemeral_storage"`
+	AllocatableMemory           float64           `db:"allocatable_memory"`
+	AllocatablePods             int64             `db:"allocatable_pods"`
+	Annotations                 map[string]string `db:"annotations"`
+	Architecture                string            `db:"architecture"`
+	CapacityCPU                 float64           `db:"capacity_cpu"`
+	CapacityMemory              float64           `db:"capacity_memory"`
+	ContainerRuntimeVersion     string            `db:"container_runtime_version"`
+	CPUUtilisation              float64           `db:"cpu_utilisation"`
+	CreationTimestamp           time.Time         `db:"creation_ts"`
+	Images                      ImageList
+	InstanceType                string  `db:"instance_type"`
+	KernelVersion               string  `db:"kernel_version"`
+	KubeletVersion              string  `db:"kubelet_version"`
+	MemoryUtilisation           float64 `db:"memory_utilisation"`
+	OperatingSystem             string  `db:"operating_system"`
+	OSImage                     string  `db:"os_image"`
+	Taints                      TaintList
 }
 
 type InfoList []*Info
